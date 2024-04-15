5cf629cdf940fb821a71b9b2bd4ae0c78f2297b1
Olivia Darby <oliviadarby@live.co.uk>
2024-04-15 12:58:33 +0100

    successfully using the GET new matches response to post the matches to the db

--- controllers/matches.py
+++ controllers/matches.py
@@ -3,7 +3,7 @@ import logging
 from datetime import datetime, timezone, timedelta
 import os
 import pprint
-from flask import Blueprint, g
+from flask import Blueprint, g, request
 from marshmallow import ValidationError, Schema, fields
 from sqlalchemy.exc import SQLAlchemyError
 from serpapi import GoogleSearch
@@ -61,7 +61,7 @@ def get_new_matches():
 
     def fetch_matches():
         params = {
-            "q": "Fulham",
+            "q": "Luton Town",
             "api_key": API_KEY,
         }
         search = GoogleSearch(params)
@@ -123,99 +123,41 @@ def get_new_matches():
 @secure_route
 def create():
 
-    premier_league_clubs = [
-        "Arsenal",
-        "Aston Villa",
-        "Bournemouth",
-        "Brentford",
-        "Brighton & Hove Albion",
-        "Burnley",
-        "Chelsea",
-        "Crystal Palace",
-        "Everton",
-        "Fulham",
-        "Liverpool",
-        "Luton Town",
-        "Manchester City",
-        "Manchester United",
-        "Newcastle United",
-        "Nottingham Forest",
-        "Sheffield United",
-        "Tottenham Hotspur",
-        "West Ham United",
-        "Wolverhampton Wanderers",
-    ]
-
-    def fetch_matches(club):
-        params = {
-            "q": club,
-            "api_key": API_KEY,
-        }
-        search = GoogleSearch(params)
-        results = search.get_dict()
-        if "sports_matches" in results.keys():
-            matches = results["sports_results"]["games"]
-
-            # pprint.pp(matches)
-            premier_league_matches = [
-                match for match in matches if match["tournament"] == "Premier League"
-            ]
-            # pprint.pp(premier_league_matches)
-            for element in premier_league_matches:
-                date = element["date"]
-                teams = element["teams"]
-                team_one = teams[0]["name"]
-                team_two = teams[1]["name"]
-                if "score" in teams[0].keys():
-                    team_one_score = teams[0]["score"]
-                    team_two_score = teams[1]["score"]
-            return (date, team_one, team_two)
-
-    def date_of_match(string):
-        if (
-            "Mon" in string
-            or "Tue" in string
-            or "Wed" in string
-            or "Thu" in string
-            or "Fri" in string
-            or "Sat" in string
-            or "Sun" in string
-        ):
-            return datetime.strptime(string, "%a, %b %d").replace(
-                year=datetime.now().year
-            )
-        else:
-            return datetime.strptime(string, "%b %d").replace(year=datetime.now().year)
-
-    # match_dictionary = request.json
+    match_dictionary = request.json
 
     try:
-        for club in premier_league_clubs:
-            data = fetch_matches(club)
+        for match in match_dictionary:
 
-            # print(data)
-            date = data[0]
-            team_one = data[1]
-            team_two = data[2]
+            # we need some logic to check if each object in match['teams'] contains a key 'score'
+            # if yes, we need to assign the value to team_one_score and team_two_score
+            # if not, the scores should be None
+            filtered_scores = []
 
-            # check if there is a score and if so, input into the api
+            for team in match["teams"]:  # here, we have identified each team object
+                if "score" in team:
+                    filtered_scores.append(int(team["score"]))
 
-            date_created = str(datetime.now(timezone.utc))
-            match_date = str(date_of_match(date))
-            # print(type(date_created), type(match_date))
+            if filtered_scores:
+                team_one_score = filtered_scores[0]
+                team_two_score = filtered_scores[1]
+            else:
+                team_one_score = None
+                team_two_score = None
 
             match_model = match_serializer.load(
                 {
-                    "match_date": match_date,
-                    "date_created": date_created,
-                    "team_one_name": team_one,
-                    "team_two_name": team_two,
+                    "match_date": match["date"],
+                    "date_created": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+                    "team_one_name": match["teams"][0]["name"],
+                    "team_one_score": team_one_score,
+                    "team_two_score": team_two_score,
+                    "team_two_name": match["teams"][1]["name"],
                 }
             )
             # print(match_model.match_date)
             db.session.add(match_model)
             db.session.commit()
-        return match_serializer.jsonify(match_model), HTTPStatus.OK
+        return {"message": "Matches successfully posted to the db"}, HTTPStatus.OK
 
     except ValidationError as e:
         return {
