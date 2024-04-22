f4a8374a6236fafe5574852e4e886c6a9d532227
Olivia Darby <oliviadarby@live.co.uk>
2024-04-22 17:07:09 +0100

    added accuracy score router

--- controllers/users.py
+++ controllers/users.py
@@ -8,6 +8,8 @@ import jwt
 from marshmallow import ValidationError
 from app import db
 from models.user import UserModel
+from models.prediction import PredictionModel
+from models.match import MatchModel
 from middleware.secure_route import secure_route
 
 user_serializer = UserSerializer()
@@ -94,11 +96,76 @@ def edit_user(user_id):
             "message": "You have missed a required field. Please try again",
         }, HTTPStatus.UNPROCESSABLE_ENTITY
 
-@router.route('/users', methods=["GET"])
+
+@router.route("/users", methods=["GET"])
 @secure_route
 def get_all_users():
     try:
         users = UserModel.query.all()
-        return user_serializer.jsonify(users, many=True), HTTPStatus.OK        
+        return user_serializer.jsonify(users, many=True), HTTPStatus.OK
     except SQLAlchemyError:
-        return {"message" : "There has been an error"}
+        return {"message": "There has been an error"}
+
+
+@router.route("/accuracy/<int:user_id>", methods=["GET"])
+@secure_route
+def get_accuracy_score(user_id):
+    try:
+        predictions = PredictionModel.query.all()
+        # print(predictions)
+        predictions_by_user = []
+        for prediction in predictions:
+            if prediction.user_id == user_id:
+                predictions_by_user.append(prediction)
+        accuracy_list = []
+        for prediction in predictions_by_user:
+            match = db.session.query(MatchModel).get(prediction.match_id)
+            if (
+                prediction.team_one_score == match.team_one_score
+                and prediction.team_two_score == match.team_two_score
+            ):
+                accuracy_list.append(1)
+        print(accuracy_list)
+        return {
+            "number-of-accurate-games": sum(accuracy_list),
+            "games_guessed": len(predictions_by_user),
+            "accuracy_score": sum(accuracy_list) / len(predictions_by_user) * 100,
+        }
+
+    except SQLAlchemyError:
+        return {"message": "There has been an error"}
+
+@router.route("/all_accuracy", methods=["GET"])
+@secure_route
+def get_all_accuracy():
+    try:
+        users = UserModel.query.all()
+        user_info = []
+        for user in users:
+            predictions = PredictionModel.query.all()
+            # print(predictions)
+            predictions_by_user = []
+            for prediction in predictions:
+                if prediction.user_id == user.id:
+                    predictions_by_user.append(prediction)
+            accuracy_list = []
+            for prediction in predictions_by_user:
+                match = db.session.query(MatchModel).get(prediction.match_id)
+                if (
+                    prediction.team_one_score == match.team_one_score
+                    and prediction.team_two_score == match.team_two_score
+                ):
+                    accuracy_list.append(1)
+            user_info.append(
+                { "user_id" : user.id,
+                    "number-of-accurate-games": sum(accuracy_list),
+                    "games_guessed": len(predictions_by_user),
+                    "accuracy_score": sum(accuracy_list)
+                    / len(predictions_by_user)
+                    * 100,
+                }
+            )
+        return {"message" : user_info}
+
+    except SQLAlchemyError:
+        return {"message": "There has been an error"}
